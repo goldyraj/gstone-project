281f05797a51cbe45b60185a718b230ad61cd1a4
Niklesh <niklesh.soni@cognicsys.com>
2017-11-20 18:20:24 +0530

    profile cha

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -12,7 +12,7 @@ export class ProfileComponent implements OnInit {
 
   rowDataIndex;
   public changePassForm: FormGroup; // our model driven form
-  public myFormEdit: FormGroup;
+  public editProfileForm: FormGroup;
   public submitted: boolean; // keep track on whether form is submitted
   public vendorList: boolean; // keep track on whether form is submitted
   public submittedEdit: boolean; // keep track on whether form is submitted
@@ -28,7 +28,6 @@ export class ProfileComponent implements OnInit {
   constructor(private _fb: FormBuilder, private http: Http, ) {
     this.access_token = localStorage.getItem("user_token");
     this.user_name = localStorage.getItem("user_name");
-    this.getUserList();
   }
 
   ngOnInit() {
@@ -37,6 +36,17 @@ export class ProfileComponent implements OnInit {
       newPass: new FormControl('', [<any>Validators.required]),
       confirPass: new FormControl('', [<any>Validators.required]),
     });
+    this.editProfileForm = new FormGroup({
+      name: new FormControl('', [<any>Validators.required]),
+      contact: new FormControl('', [<any>Validators.required]),
+      pan_no: new FormControl('', [<any>Validators.required]),
+      username: new FormControl('', [<any>Validators.required]),
+      gstin: new FormControl('', [<any>Validators.required]),
+      address: new FormControl('', [<any>Validators.required]),
+      state: new FormControl('', [<any>Validators.required]),
+      city: new FormControl('', [<any>Validators.required]),
+    });
+    this.getUserList();
   }
 
 
@@ -82,9 +92,63 @@ export class ProfileComponent implements OnInit {
     this.http.get('http://localhost:3000/api/auth/check?token=' + this.access_token).subscribe(data => {
       this.userDetail = data.json().info;
       console.log("userDetail", this.userDetail);
+      console.log("name", this.userDetail['name']);
+      this.editProfileForm.get("name").setValue(this.userDetail['name']);
+      this.editProfileForm.get("contact").setValue(this.userDetail['contact']);
+      this.editProfileForm.get("pan_no").setValue(this.userDetail['pan_no']);
+      this.editProfileForm.get("username").setValue(this.userDetail['username']);
+      this.editProfileForm.get("gstin").setValue(this.userDetail['gstin']);
+      this.editProfileForm.get("address").setValue(this.userDetail['address']);
+      this.editProfileForm.get("state").setValue(this.userDetail['state']);
+      this.editProfileForm.get("city").setValue(this.userDetail['city']);
     });
   }
 
+  editProfile(isValid: boolean) {
+    this.submittedEdit = true; // set form submit to true
+    console.log(isValid);
+    console.log("hi form module is called from page");
+    console.log("edited form data", this.editProfileForm.value);
+
+    // if (isValid == true && this.myFormEdit.value.selectedstateDropdown!='Select State') {
+    // if (isValid == true) {
+
+    //   const headers = new Headers();
+
+    //   headers.append('Content-Type', 'application/json');
+    //   // headers.append('x-access-token', this.access_token);
+    //   const requestOptions = new RequestOptions({ headers: headers });
+
+    //   const body = {
+    //     "_id": this.venderRowData._id,
+    //     "name": this.myFormEdit.value.name,
+    //     "gstin": this.myFormEdit.value.gstin,
+    //     "address": this.myFormEdit.value.address,
+    //     "state": this.selectedState
+    //   };
+    //   console.log("body", body);
+
+    //   this.url = "http://localhost:3000/api/vendor/update?token=" + this.access_token;
+    //   return this.http.put(this.url, body, requestOptions)
+    //     .subscribe(
+    //     response => {
+    //       console.log("suceessfull data", response.json().message);
+    //       this.closeEditModal();
+    //       this.submittedEdit = false;
+    //       this.getVenderList(this.pager.currentPage);
+    //       // this.hsnCodeData.push(body);
+
+    //       this.venderList[this.rowDataIndex] = body;
+    //       // alert(response.json().message);
+    //     },
+    //     error => {
+    //       // this.closeEditModal();
+    //       console.log("error", error.message);
+    //       console.log(error.text());
+    //     }
+    //     );
+    // }
+  }
 
   saveTodos(val) {
     //show box msg

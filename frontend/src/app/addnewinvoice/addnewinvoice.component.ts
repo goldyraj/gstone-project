bed0bb54404366fc1ffe4fb76e68cd5595168437
mayur saini <mayursaini1987@gmail.com>
2017-11-22 19:04:57 +0530

    invoice add

--- frontend/src/app/addnewinvoice/addnewinvoice.component.ts
+++ frontend/src/app/addnewinvoice/addnewinvoice.component.ts
@@ -21,7 +21,10 @@ export class AddnewinvoiceComponent implements OnInit {
   filteredList = [];
   query: string;
   invoiceTypeRadioForm: FormGroup;
-  customerDetailsList=[];
+  customerDetailsList = [];
+  userGstin="ABCDE123R";
+  grandTotal:number=1000000;
+  stateList=[];
 
   serice = [
     'SELECT',
@@ -90,50 +93,10 @@ export class AddnewinvoiceComponent implements OnInit {
     'OTH-OTHERS'
   ]
 
-  place = [
-    'SELECT',
-    '01-Jammu & Kashmir',
-    '02-Himachal Pradesh',
-    '03-Punjab',
-    '04-Chandigarh',
-    '05-Uttarakhand',
-    '06-Haryana',
-    '07-Delhi',
-    '08-Rajasthan',
-    '09-Uttar Pradesh',
-    '10-Bihar',
-    '11-Sikkim',
-    '12-Arunachal Pradesh',
-    '13-Nagaland',
-    '14-Manipur',
-    '15-Mizoram',
-    '16-Tripura',
-    '17-Meghalaya',
-    '18-Assam',
-    '19-West Bengal',
-    '20-Jharkhand',
-    '21-Odisha',
-    '22-Chhattisgarh',
-    '23-Madhya Pradesh',
-    '24-Gujarat',
-    '25-Daman & Diu',
-    '26-Dadra & Nagar Haveli',
-    '27-Maharashtra',
-    '29-Karnataka',
-    '30-Goa',
-    '31-Lakshdweep',
-    '32-Kerala',
-    '33-Tamil Nadu',
-    '34-Pondicherry',
-    '35-Andaman & Nicobar Islands',
-    '36-Telangana',
-    '37-Andhra Pradesh',
-    '97-Other Territory'
-  ]
-
   customerDetailsForm: FormGroup;
-  selectedCustomerData:any;
-  disableEcommerceInput:boolean=false;
+  selectedCustomerData: any;
+  disableEcommerceInput: boolean = false;
+  invoiceTypeRadioArray=[];
 
   constructor(public http: Http, private pagerService: PagerService, private router: Router) {
 
@@ -146,9 +109,14 @@ export class AddnewinvoiceComponent implements OnInit {
       invoiceNo: new FormControl('', [<any>Validators.required]),
       date: new FormControl('', [<any>Validators.required]),
       gstin: new FormControl('', [<any>Validators.required]),
-      pos: new FormControl('', [<any>Validators.required]),
+      pos: new FormControl('0', [<any>Validators.required]),
       eComGstin: new FormControl('', [<any>Validators.required]),
     });
+  }
+
+  createInvoiceTypeRadioArray()
+  {
+    this.invoiceTypeRadioArray.push()
   }
 
   ngOnInit() {
@@ -159,6 +127,7 @@ export class AddnewinvoiceComponent implements OnInit {
 
     this.access_token = localStorage.getItem('user_token');
 
+    this.getStateList();
     this.getCustomerNames();
   }
 
@@ -177,7 +146,7 @@ export class AddnewinvoiceComponent implements OnInit {
       response => {
         // this.customersNamesList = response.json().docs;
         console.log("RESPONSE", response.json().docs);
-        this.customerDetailsList=response.json().docs;
+        this.customerDetailsList = response.json().docs;
         for (let data of response.json().docs) {
           console.log("NAMES", data.name);
           this.customersNamesList.push(data.name);
@@ -206,26 +175,23 @@ export class AddnewinvoiceComponent implements OnInit {
   select(item) {
     this.query = item;
     this.filteredList = [];
-    this.selectedCustomerData= this.customerDetailsList.filter(x => x.name == item);
-    console.log("NAME",this.selectedCustomerData);
-    console.log("GSTIN",this.selectedCustomerData.gstin);
+    this.selectedCustomerData = this.customerDetailsList.filter(x => x.name == item);
+    console.log("NAME", this.selectedCustomerData);
+    console.log("GSTIN", this.selectedCustomerData.gstin);
     this.customerDetailsForm.get('gstin').setValue(this.selectedCustomerData[0].gstin);
   }
 
   selectRadio() {
-    if(this.invoiceTypeRadioForm.controls.invoiceTypeRadio.value==="E_Commerce")
-    {
-      this.disableEcommerceInput=true;
+    if (this.invoiceTypeRadioForm.controls.invoiceTypeRadio.value === "E_Commerce") {
+      this.disableEcommerceInput = true;
     }
-    else
-    {
-      this.disableEcommerceInput=false;
+    else {
+      this.disableEcommerceInput = false;
     }
-    console.log("CLICKEED",this.disableEcommerceInput);
+    console.log("CLICKEED", this.disableEcommerceInput);
   }
 
-  addInvoice()
-  {
+  addInvoice() {
     let response: any;
     let myHeaders = new Headers({ 'Content-Type': 'application/json' });
     myHeaders.append('Access-Control-Allow-Headers', 'origin, content-type, accept, authorization, x-access-token');
@@ -235,57 +201,108 @@ export class AddnewinvoiceComponent implements OnInit {
 
     let options = new RequestOptions({ headers: myHeaders });
 
-    let itm_det={
-      "rt":"",
-      "txval":"",
-      "iamt":"",
-      "csamt":""
-    };
-
-    let items ={
-      "num":"",
-      // "itm_det":JSON.parse(itm_det)
-    };
-
-    let invArray={
-        // "inum":this.customerDetailsForm.controls.invoiceNo.value,
-        // "idt":this.customerDetailsForm.controls.date.value,
-        // "val":this.customerDetailsForm.controls.
-        // "pos":"",
-        // "rchrg":"",
-        // "etin":"",
-        // "inv_typ":"",
-        // "itms":JSON.parse()
-    };
-
-    // let b2bArray={
-    //   "ctin":this.customerDetailsForm.controls.gstin.value,
-    //   "inv"
+    // const body = {
+    //   "gstin": this.userGstin,
+    //   "fp": this.customerDetailsForm.controls.date.value,
+    //   "gt": this.customerDetailsForm.controls.gstin.value,
+    //   "cur_gt": this.customerDetailsForm.controls.gstin.value,
+    //   "inum": this.customerDetailsForm.controls.invoiceNo.value,
+    //   "b2b": [
+    //     {
+    //       "ctin": this.customerDetailsForm.controls.gstin.value,
+    //       "inv": [
+    //         {
+    //           "inum":this.customerDetailsForm.controls.invoiceNo.value,
+    //           "idt": this.customerDetailsForm.controls.date.value,
+    //           "val": this.grandTotal,
+    //           "pos": this.customerDetailsForm.controls.pos,
+    //           "rchrg": "N",
+    //           "etin": this.customerDetailsForm.controls.gstin,
+    //           "inv_typ": "R",
+    //           "itms": [
+    //             {
+    //               "num": 1,
+    //               "itm_det": {
+    //                 "rt": 5,
+    //                 "txval": 10000,
+    //                 "iamt": 833.33,
+    //                 "csamt": 500
+    //               }
+    //             }
+    //           ]
+    //         }
+    //       ]
+    //     }
+    //   ]
     // };
 
-    let body={
-      "gstin":this.customerDetailsForm.controls.gstin.value,
-      "fp":"",
-      "gt":this.customerDetailsForm.controls.gstin.value,
-      "cur_gt":this.customerDetailsForm.controls.gstin.value,
-      "inum":this.customerDetailsForm.controls.gstin.value,
-      "b2b":this.customerDetailsForm.controls.gstin.value,
-      // "inum":this.customerDetailsForm.controls.gstin.value,
-      // "inum":this.customerDetailsForm.controls.gstin.value,
-      // "inum":this.customerDetailsForm.controls.gstin.value,
-      // "inum":this.customerDetailsForm.controls.gstin.value,
-
+    const body={
+      "gstin": "anil",
+      "fp": "kjdskfkj392",
+      "gt":"sadfd34543543df",
+      "cur_gt":3748578584735,
+      "inum": "S008300",
+      "b2b": [
+      {
+        "ctin": "01AABCE2207R1Z5",
+        "inv": [
+          {
+            "inum": "S008400",
+            "idt": "24-11-2016",
+            "val": 729248.16,
+            "pos": "06",
+            "rchrg": "N",
+            "etin": "01AABCE5507R1C4",
+            "inv_typ": "R",
+            "itms": [
+              {
+                "num": 1,
+                "itm_det": {
+                  "rt": 5,
+                  "txval": 10000,
+                  "iamt": 833.33,
+                  "csamt": 500
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
     };
 
-    this.http.post('http://localhost:3000/api/invoice/create?token=' + this.access_token + '&limit=' + 5000,body, options)
+    console.log("BODY",body);
+
+    this.http.post('http://localhost:3000/api/invoice/create?token=' + this.access_token, body, options)
       .subscribe(
       response => {
-        console.log("RESPONSE", response.json().docs);
-        this.customerDetailsList=response.json().docs;
-        for (let data of response.json().docs) {
-          this.customersNamesList.push(data.name);
-          console.log("CUSTOMER_NAMES", this.customersNamesList);
-        }
+        console.log("RESPONSE", response);
+        // this.customerDetailsList = response.json().docs;
+        // for (let data of response.json().docs) {
+        //   this.customersNamesList.push(data.name);
+        //   console.log("CUSTOMER_NAMES", this.customersNamesList);
+        // }
+      },
+      error => {
+        console.log(response);
+      }
+      );
+  }
+
+  getStateList() {
+    let response: any;
+    let myHeaders = new Headers({ 'Content-Type': 'application/json' });
+    myHeaders.append('Access-Control-Allow-Headers', 'origin, content-type, accept, authorization, x-access-token');
+    myHeaders.append('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
+    myHeaders.append('Access-Control-Allow-Origin', '*');
+    myHeaders.append('Access-Control-Allow-Credentials', 'true');
+
+    let options = new RequestOptions({ headers: myHeaders });
+
+    this.http.get('http://localhost:3000/api/state/list?token=' + this.access_token + '&limit=' + 1000, options)
+      .subscribe(
+      response => {
+        this.stateList = response.json().state;
       },
       error => {
         console.log(error.text());

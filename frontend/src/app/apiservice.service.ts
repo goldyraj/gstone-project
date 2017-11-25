8a69c9f3ad39d1fc56202941cadc1c1a0f6b0df4
mayur saini <mayursaini1987@gmail.com>
2017-11-25 14:46:01 +0530

    m

--- frontend/src/app/apiservice.service.ts
+++ frontend/src/app/apiservice.service.ts
@@ -4,7 +4,7 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 @Injectable()
 export class ApiserviceService {
 
-  // public BASE_URL="http://localhost:3000/api/";
-  public BASE_URL="http://165.227.183.172:3000/api/";
+  public BASE_URL="http://localhost:3000/api/";
+  // public BASE_URL="http://165.227.183.172:3000/api/";
   constructor(public http: Http) { }
 }
